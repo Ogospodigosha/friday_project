773d7ee2c0a044c8f996558f63a7fa670e95b41f
Ogospodigosha <odintsovis@hotmail.com>
2022-12-10 15:06:55 +0300

    add: eslint && prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.css';
-import {Header} from "./components/header/Header";
-import {Pages} from "./components/pages/Pages";
+import React from 'react'
+
+import './App.css'
+import { Header } from './components/header/Header'
+import { Pages } from './components/pages/Pages'
 
 function App() {
-    return (
-        <div>
-            <Header/>
-            <Pages/>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <Pages />
+    </div>
+  )
 }
 
-export default App;
+export default App
