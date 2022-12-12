151678db027076e7ed2c8052b05718956890d620
Kostya Lapatin <klrotex11@gmail.com>
2022-12-12 15:06:40 +0300

    add: useAppSelector, useAppDispatch

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,8 @@
-import { combineReducers, legacy_createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+
+import { TLoginReducer } from '../features/auth/login/login-reducer'
 
 import { loadingReducer } from './loadingReducer'
 
@@ -8,3 +11,20 @@ export const RootReducer = combineReducers({
 })
 export const store = legacy_createStore(RootReducer, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof RootReducer>
+
+// create custom hook
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+// types
+// all action type
+type ActionType = TLoginReducer
+
+// universal thunk type
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppRootStateType,
+  unknown,
+  ActionType
+>
+type AppThunkDispatch = ThunkDispatch<AppRootStateType, unknown, ActionType>
