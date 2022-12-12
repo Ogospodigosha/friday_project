532d4d601e3b5909a1b9dd31ec4496f6802f8973
Kostya Lapatin <klrotex11@gmail.com>
2022-12-12 15:07:21 +0300

    fix: index.ts import

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,4 +1,4 @@
-import axios from 'axios/index'
+import axios from 'axios'
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
--- src/app/store.ts
+++ src/app/store.ts
@@ -2,21 +2,22 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
 import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
-import { TLoginReducer } from '../features/auth/login/login-reducer'
+import { loginReducer, TLoginReducer } from '../features/auth/login/login-reducer'
 
 import { loadingReducer } from './loadingReducer'
 
 export const RootReducer = combineReducers({
   loading: loadingReducer,
+  login: loginReducer,
 })
 export const store = legacy_createStore(RootReducer, applyMiddleware(thunk))
-export type AppRootStateType = ReturnType<typeof RootReducer>
 
 // create custom hook
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // types
+export type AppRootStateType = ReturnType<typeof RootReducer>
 // all action type
 type ActionType = TLoginReducer
 
