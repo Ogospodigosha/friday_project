755ccfe56ebc51c593462696fdd12437894c4521
Ogospodigosha <odintsovis@hotmail.com>
2022-12-17 16:10:48 +0300

    fix: link Profile to Packs

--- src/components/pages/Pages.tsx
+++ src/components/pages/Pages.tsx
@@ -8,8 +8,8 @@ import { ForgotPassword } from '../../features/auth/forgotPassword/ForgotPasswor
 import { Login } from '../../features/auth/login/Login'
 import { Profile } from '../../features/auth/profile/Profile'
 import { Registration } from '../../features/auth/registration/Registration'
+import { Packs } from '../../features/cards/packs/Packs'
 
-// import { TestForm } from '../../features/auth/registration/TestForm'
 import { Error404 } from './error404/Error404'
 
 export const PATH = {
@@ -19,6 +19,7 @@ export const PATH = {
   CREATE_NEW_PASSWORD: '/createNewPassword/',
   REGISTRATION: '/registration',
   CHECK_EMAIL: '/check_email',
+  PACKS: '/packs',
 }
 export const Pages = () => {
   return (
@@ -28,7 +29,7 @@ export const Pages = () => {
       <Route path={PATH.REGISTRATION} element={<Registration />} />
       <Route path={PATH.PROFILE} element={<Profile />} />
       <Route path={PATH.FORGOT_PASSWORD} element={<ForgotPassword />} />
-
+      <Route path={PATH.PACKS} element={<Packs />} />
       <Route path={PATH.CREATE_NEW_PASSWORD} element={<CreateNewPassword />}>
         <Route path=":token" element={<CreateNewPassword />} />
       </Route>
